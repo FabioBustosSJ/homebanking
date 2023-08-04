5757d67aa8cc9040ebc05bba61e47dc205aef276
FabioBustosSJ <febt2002@hotmail.com>
2023-08-04 16:12:27 -0300

    Proyecto homebanking

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,35 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+
+			Client client1 = new Client();
+
+			client1.setEmail("pepe@ponpin.es");
+			client1.setFirstName("Pepe");
+			client1.setLastName("Pompin");
+
+			Client client2 = new Client("fer@palla","Fermin", "Pallares");
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+
+		});
+	}
+}
